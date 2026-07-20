f3da0dc52fc33d7d23b07c9bdafbba9b74b666f6
Adam Mattioli <amattiolidev@gmail.com>
2026-07-19 22:22:35 -0400

    feature/EngineDev refactoring the TextureAtlas to follow single responsibility principle better. Added new TextureAtlasLoader class with internal helpers for modeling new JSON schema and replacing XML atlas files with JSON.

--- Graphics/TextureAtlas.cs
+++ Graphics/TextureAtlas.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using System;
+﻿using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.IO;
-using System.Xml;
-using System.Xml.Linq;
 
 namespace ChefEngine.Graphics
 {
@@ -22,16 +16,6 @@ namespace ChefEngine.Graphics
 
         // The overall texture that contains all the regions.
         public Texture2D Texture { get; private set; }
-
-        /// <summary>
-        /// Empty constructor for the TextureAtlas class.
-        /// </summary>
-        public TextureAtlas()
-        {
-            // Initialize the texture regions and animations dictionaries.
-            _textureRegions = new Dictionary<string, TextureRegion>();
-            _animations = new Dictionary<string, Animation>();
-        }
 
         /// <summary>
         /// Constructor for the TextureAtlas class.
@@ -129,146 +113,6 @@ namespace ChefEngine.Graphics
         {
             // Clear the dictionary of animations.
             _animations.Clear();
-        }
-
-        /// <summary>
-        /// Static method for creating a texture atlas from an xml config file.
-        /// </summary>
-        /// <param name="content">The content manager instance for loading assets.</param>
-        /// <param name="fileName">The file name of the xml config file.</param>
-        /// <returns>TextureAtlas object created from the provided file.</returns>
-        public static TextureAtlas CreateFromFile(ContentManager content, string fileName)
-        {
-            // Create a new texture atlas instance.
-            TextureAtlas atlas = new TextureAtlas();
-
-            // Get the file path of the xml config file.
-            string filePath = Path.Combine(content.RootDirectory, fileName);
-
-            using (Stream stream = TitleContainer.OpenStream(filePath))
-            {
-                using (XmlReader reader = XmlReader.Create(stream))
-                {
-                    // Load and parse the root from the xml config file.
-                    XDocument doc = XDocument.Load(reader);
-                    XElement root = doc.Root;
-
-                    // Get the texture path from the <Texture> element
-                    string texturePath = root.Element("Texture").Value;
-
-                    // Load the texture using the content manager and assign it to the atlas.
-                    atlas.Texture = content.Load<Texture2D>(texturePath);
-
-                    // Get the <Regions> element and its child <Region> elements
-                    IEnumerable<XElement> regions = root.Element("Regions")?.Elements("Region");
-
-                    // If the regions element is not null
-                    if (regions != null)
-                    {
-                        // For each region element
-                        foreach (XElement region in regions)
-                        {
-                            // Get the attributes of the region element and parse them into variables.
-                            string name = region.Attribute("name")?.Value;
-                            int x = int.Parse(region.Attribute("x")?.Value ?? "0");
-                            int y = int.Parse(region.Attribute("y")?.Value ?? "0");
-                            int width = int.Parse(region.Attribute("width")?.Value ?? "0");
-                            int height = int.Parse(region.Attribute("height")?.Value ?? "0");
-
-                            // If the region has a valid name.
-                            if (!string.IsNullOrEmpty(name))
-                            {
-                                // Add the region to the atlas using the parsed values.
-                                atlas.AddRegion(name, x, y, width, height);
-                            }
-                        }
-                    }
-
-                    // Get the <Animations> element and its child <Animation> elements
-                    IEnumerable<XElement> animations = root.Element("Animations")?.Elements("Animation");
-
-                    // If the animations element is not null
-                    if (animations != null)
-                    {
-                        // For each animation element
-                        foreach (XElement animation in animations)
-                        {
-                            // Get the name and delay attributes and parse them into variables.
-                            string name = animation.Attribute("name")?.Value;
-                            float delayMs = float.Parse(animation.Attribute("delay")?.Value ?? "0");
-                            TimeSpan delayMsTimespan = TimeSpan.FromMilliseconds(delayMs);
-
-                            // Initialize the frame region list
-                            List<TextureRegion> frameRegionList = new List<TextureRegion>();
-
-                            // Get the <Animation> element's child <Frame> elements
-                            IEnumerable<XElement> frames = animation.Elements("Frame");
-
-                            // If the frames element list is not null
-                            if (frames != null)
-                            {
-                                // For each frame element
-                                foreach (XElement frame in frames)
-                                {
-                                    // Get the region attribute and parse it into a variable.
-                                    string frameRegion = frame.Attribute("region")?.Value;
-
-                                    // If the frame has a valid region.
-                                    if (!string.IsNullOrEmpty(frameRegion))
-                                    {
-                                        // Create the texture region
-                                        TextureRegion textureRegion = atlas.GetRegion(frameRegion);
-
-                                        // Add the texture region to the frame region list.
-                                        frameRegionList.Add(textureRegion);
-                                    }
-                                }
-                            }
-
-                            // If the animation has a valid name.
-                            if (!string.IsNullOrEmpty(name))
-                            {
-                                // Create the animation
-                                Animation animationToAdd = new Animation(frameRegionList, delayMsTimespan);
-
-                                // Add the animation to the atlas using the parsed values.
-                                atlas.AddAnimation(name, animationToAdd);
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Return the atlas.
-            return atlas;
-        }
-
-        /// <summary>
-        /// Creates a sprite from a region in the texture atlas.
-        /// </summary>
-        /// <param name="textureRegionName">The name of the texture region to create the sprite from.</param>
-        /// <returns>Sprite using the texture region as it's source.</returns>
-        public Sprite CreateSprite(string textureRegionName)
-        {
-            // Get the texture region from the atlas.
-            TextureRegion region = GetRegion(textureRegionName);
-
-            // Create and return the sprite.
-            return new Sprite(region);
-        }
-
-        /// <summary>
-        /// Creates an animated sprite from an animation in the texture atlas.
-        /// </summary>
-        /// <param name="animationName">The name of the animation to create the sprite from.</param>
-        /// <returns>Animated sprite using the animation as it's source.</returns>
-        public AnimatedSprite CreateAnimatedSprite(string animationName)
-        {
-            // Get the animation from the atlas.
-            Animation animation = GetAnimation(animationName);
-
-            // Create and return the animated sprite.
-            return new AnimatedSprite(animation);
         }
     }
 }
---
+++ Graphics/TextureAtlasLoader.cs
@@ -0,0 +1,98 @@
+﻿using ChefEngine.Core;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ChefEngine.Graphics
+{
+    /// <summary>
+    /// TextureAtlasLoader static class for loading in texture atlas files.
+    /// </summary>
+    public static class TextureAtlasLoader
+    {
+        /// <summary>
+        /// Static method for loading a texture atlas in from a json file.
+        /// </summary>
+        /// <param name="fileName">The file name of the json file.</param>
+        /// <returns>TextureAtlas object loaded from the provided file.</returns>
+        public static TextureAtlas Load(string fileName)
+        {
+            // Get the file path of the json file.
+            string filePath = Path.Combine(Engine.Instance.Content.RootDirectory, fileName);
+
+            // Read and deserialize the json into data classes.
+            string json = File.ReadAllText(filePath);
+            TextureAtlasData data = JsonSerializer.Deserialize<TextureAtlasData>(
+                json,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            // Load the referenced texture using the content manager.
+            Texture2D texture = Engine.Instance.Content.Load<Texture2D>(data.Texture);
+
+            // Create the texture atlas instance.
+            TextureAtlas atlas = new TextureAtlas(texture);
+
+            // For each texture region in the atlas file.
+            foreach (TextureAtlasRegionData region in data.Regions)
+            {
+                // Add the texture region.
+                atlas.AddRegion(region.Name, region.X, region.Y, region.Width, region.Height);
+            }
+
+            // For each animation in the atlas file.
+            foreach (TextureAtlasAnimationData animation in data.Animations)
+            {
+                // Initialize the list of animation frames.
+                List<TextureRegion> animationFrames = new List<TextureRegion>();
+                
+                // For each animation frame.
+                foreach (string frame in  animation.Frames)
+                {
+                    // Add the texture region.
+                    animationFrames.Add(atlas.GetRegion(frame));
+                }
+
+                // Add the animation.
+                atlas.AddAnimation(animation.Name, new Animation(animationFrames, TimeSpan.FromMilliseconds(animation.Delay)));
+            }
+
+            // Return the atlas.
+            return atlas;
+        }
+    }
+
+    /// <summary>
+    /// TextureAtlasData internal class for modeling top level object in texture atlas json file.
+    /// </summary>
+    internal class TextureAtlasData
+    {
+        public string Texture { get; set; }
+        public List<TextureAtlasRegionData> Regions { get; set; }
+        public List<TextureAtlasAnimationData> Animations { get; set; }
+    }
+
+    /// <summary>
+    /// TextureAtlasRegionData internal class for modeling regions array items in texture atlas json file.
+    /// </summary>
+    internal class TextureAtlasRegionData
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    /// <summary>
+    /// TextureAtlasAnimationdata internal class for modeling animations array items in texture atlas json file.
+    /// </summary>
+    internal class TextureAtlasAnimationData
+    {
+        public string Name { get; set; }
+        public float Delay { get; set; }
+        public List<string> Frames { get; set; }
+    }
+}
